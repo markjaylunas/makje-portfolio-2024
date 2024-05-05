ecefb203245e6fadacaa8ee470b8d2db4f85205d
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-05 13:27:16 +0800

    add nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "./lib/utils";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          inter.className,
+          "dark text-foreground bg-background min-h-screen"
+        )}
+      >
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
