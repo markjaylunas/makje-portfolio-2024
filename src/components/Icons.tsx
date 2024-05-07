1ad1d01d93e43f4ca9ee5d1eb03b8a4d87ba0d03
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-05-07 15:25:04 +0800

    Add not found page

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -148,4 +148,36 @@ export const Icons = {
       <path d="M19 11h2m-1 -1v2" />
     </svg>
   ),
+
+  arrowBack: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1" />
+    </svg>
+  ),
+
+  home: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </svg>
+  ),
 };
